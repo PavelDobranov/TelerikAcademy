db0f7071cdbab3d4d746cda5e5b0ce7e8397fc3d
PavelDobranov <pdobranov@gmail.com>
2015-03-24 07:39:21 +0200

    Small fix

--- 03-CSharp-Object-Oriented-Programming/OOPPrinciplesPartOne/01.SchoolClasses/Gender.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SchoolClasses
-{
-    public enum Gender
-    {
-        male,
-        female
-    }
-}
--- 03-CSharp-Object-Oriented-Programming/OOPPrinciplesPartOne/01.SchoolClasses/Interfaces/IHuman.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SchoolClasses.Interfaces
-{
-    public interface IHuman
-    {
-        string Name { get; set; }
-
-        Gender Gemder { get; set; }
-    }
-}
