5b604701f243ff02bf64fa7583be6f43743d6273
Pavel Dobranov <pdobranov@gmail.com>
2015-08-21 17:02:34 +0300

    Added "Development tools" homework

---
+++ 10-High-Quality-Code/15-DevelopmentTools/02.T4Template/TestTemplate.cs
@@ -0,0 +1,8 @@
+﻿public class TestTemplateClass
+{
+    public string SomeProp0 { get; set; }
+    public string SomeProp1 { get; set; }
+    public string SomeProp2 { get; set; }
+    public string SomeProp3 { get; set; }
+    public string SomeProp4 { get; set; }
+}
